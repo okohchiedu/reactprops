95cda8b8794b4c1d0528cf9f85f2f2f631866c8c
okohchiedu <okohthomas35@gmail.com>
2023-03-29 01:46:54 +0100

    first commit

---
+++ props/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//     <h2>welcome to functional component</h2>
+//     </div>
+//   );
+// }
+
+
+const App =() => {
+  const itemArray = [
+    {
+      
+        imgSrc: 'https://www.notebookcheck.net/fileadmin/_processed_/c/d/csm_Produktfotos_Apple_iPhone_13_Pro_1448_0001583eaa.jpg',
+        name: 'Iphone 13 Pro-max',
+        price: '#1000000'
+    
+    },
+
+    {
+      imgSrc: 'https://www.notebookcheck.net/fileadmin/_processed_/c/d/csm_Produktfotos_Apple_iPhone_13_Pro_1448_0001583eaa.jpg',
+      name: 'laptop',
+      price: '#3000000'
+    },
+   {
+    imgSrc: 'https://www.notebookcheck.net/fileadmin/_processed_/c/d/csm_Produktfotos_Apple_iPhone_13_Pro_1448_0001583eaa.jpg',
+    name: 'Iphone 13 Pro-max',
+    price: '#1000000'
+   }
+
+
+  ]
+}
+
+
+export default App;
---
+++ props/src/Component/compFun.jsx
@@ -0,0 +1,12 @@
+
+
+
+const ComFun = () => {
+    return(
+        <did>
+           <h3>welcome to functional component</h3> 
+           <h4>my name is {chiedu.name}</h4>
+           <h4>my best food is{chiedu.bestFood}{} </h4>
+        </did>
+    )
+}
